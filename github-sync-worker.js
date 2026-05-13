b3a79a9d59293da35cc560bc31c6d8c61ec65091
Wang, Xuran <xuran.wang@continental-corporation.com>
2026-05-13 15:12:18 +0800

    Fix iOS CORS: add Access-Control-Max-Age and Authorization header

--- github-sync-worker.js
+++ github-sync-worker.js
@@ -25,7 +25,8 @@ function getCorsHeaders() {
   return {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Max-Age': '86400',
   };
 }
 
